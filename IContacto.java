3c0351012cf670b10ad27d2d0b229613e564f408
Sergio <ore221122@gmail.com>
2022-11-08 09:33:49 -0600

    Se crearon las clases que se van a utilizar.

---
+++ IContacto.java
@@ -0,0 +1,20 @@
+/*
+Profesor: Lynette García
+Auxiliar: Rodrigo Barrera y Gaby Contreras
+
+INTEGRANTE: 
+Sergio Alejandro Orellana Colindres (221122)
+Andre Marroquín Tarrot (22266)
+8/11/2022
+
+Laboratorio 4
+
+Curso: Programación Orientada a Objetos
+Sección: 10
+
+Interface de los contactos del teléfono
+*/
+
+public interface IContacto {
+    
+}
